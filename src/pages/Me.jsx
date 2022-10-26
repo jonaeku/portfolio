25e8f7d495936b0b6247fdca91a5dbfcc4227fea
Jona <jonaeku@gmx.de>
2022-10-26 20:43:03 +0200

    Update: new logo + styling

---
+++ src/pages/Me.jsx
@@ -0,0 +1,10 @@
+const Me = () => {
+    return (
+        <div>
+            <h1 className="text-3xl mb-3">Technologien</h1>
+            <p>Hier bekommt Ihr einen Überblick über die Technologien die ich akutell nutze oder zumindest behersche 😉</p>
+        </div>
+    );
+}
+
+export default Me;
