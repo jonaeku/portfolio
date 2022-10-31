4992a17ecdccffe13f35763db89412a6a98ed091
Jona <jonaeku@gmx.de>
2022-10-31 12:23:07 +0100

    Add: projects and Contact Form

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,105 @@
+import React, { useState } from "react";
+import styles from "../styles/ContactForm.module.css";
+
+const ContactForm = () => {
+    const [formData, setFormData] = useState({});
+    const [isActive, setIsActive] = useState(false);
+
+    async function handleOnSubmit(e) {
+        e.preventDefault();
+
+        if (!formData.email) {
+            alert("Bitte geben Sie beim Senden einer Anfrage Ihre Email an!");
+            return;
+        } else {
+            await fetch("/mail", {
+                method: "POST",
+                body: JSON.stringify(formData),
+            });
+            setFormData({
+                name: "",
+                email: "",
+                personCount: "",
+                date: "",
+                time: "",
+                message: "",
+            });
+            setIsActive((current) => !current);
+        }
+    }
+
+    return (
+        <div className={styles.container}>
+            <div>
+                <title>Kontakt</title>
+                <meta name="description" content="Termin anfragen!" />
+                <link rel="icon" href="/favicon.ico" />
+            </div>
+
+            <div className={styles.main}>
+                <h1 class="text-3xl mb-3">Kontakt</h1>
+                <p class="text-md lg:text-xl">
+                    Gerne können wir gemeinsam auf Ihr geplantes Projekt umsetzen 😃
+                </p>
+                <br />
+                <div className={styles.mailContainer}>
+                    <form onSubmit={handleOnSubmit}>
+                        <div className={styles.grid}>
+                            <div className="flex gap-3">
+                                <p>
+                                    <label htmlFor="name">Name</label>
+                                    <br />
+                                    <input
+                                        className={styles.input}
+                                        id="name"
+                                        type="text"
+                                        name="name"
+                                        value={formData.name}
+                                        onChange={(e) =>
+                                            setFormData({ ...formData, name: e.target.value })
+                                        }
+                                    />
+                                </p>
+                                <p>
+                                    <label htmlFor="email">Email</label>
+                                    <br />
+                                    <input
+                                        className={styles.input}
+                                        id="email"
+                                        type="email"
+                                        name="email"
+                                        value={formData.email}
+                                        onChange={(e) =>
+                                            setFormData({ ...formData, email: e.target.value })
+                                        }
+                                    />
+                                </p>
+                            </div>
+                            <p className="mt-3">
+                                <label htmlFor="message">Sonstiges</label>
+                                <br />
+                                <input
+                                    className={styles.inputText}
+                                    id="message"
+                                    name="message"
+                                    value={formData.message}
+                                    onChange={(e) =>
+                                        setFormData({ ...formData, message: e.target.value })
+                                    }
+                                />
+                            </p>
+                            <button
+                                className={isActive ? styles.submitActive : styles.submit}
+                            >
+                                {isActive ? "Gesendet" : "Senden"}
+                            </button>
+                        </div>
+                    </form>
+                    {isActive ? <p className={styles.sendInfo}>Anfrage erfolgreich versendet!</p> : ""}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ContactForm;
---
+++ src/pages/Mail.js
@@ -0,0 +1,19 @@
+import sgMail from "@sendgrid/mail";
+sgMail.setApiKey(import.meta.env.PUBLIC_SENDGRID_API);
+
+export async function post({ request }) {
+  const data = await request.json()
+  await sgMail.send({
+    to: "hello@ionae.de",
+    from: "Anfrage@ionae.de",
+    templateId: "d-613fda644ea44d9eb7b1b28b8fdd1f4a",
+    dynamicTemplateData: {
+      subject: `Anfrage von ${data.email}`,
+      name: "Jona",
+      nameRequest: data.name,
+      email: data.email,
+      text: data.message,
+    },
+  });
+  return new Response(JSON.stringify({ successful: true }), { status: 200 });
+};
